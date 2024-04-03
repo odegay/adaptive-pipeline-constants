b903ac1d01623377d724605422f0928364cc2051
odegay <dolegan@gmail.com>
2024-04-03 17:37:17 -0400

    first commit 3

--- setup.py
+++ setup.py
@@ -10,6 +10,5 @@ setup(
     description="Constants for the Adaptive Pipeline Workflow project.",
     license="MIT",
     keywords="GenAI, GCP",
-    url="https://github.com/odegay/adaptive-pipeline-constants",    
-    install_requires=[],  # List any dependencies your library has
+    url="https://github.com/odegay/adaptive-pipeline-constants",            
 )
